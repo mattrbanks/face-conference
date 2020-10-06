a092bf7c4f9950ef237a90a6c08007351da35e13
mattrbanks <banks.matt81@gmail.com>
2020-10-06 13:41:02 -0400

    Added gitignore file.

--- public/script.js
+++ public/script.js
@@ -7,6 +7,7 @@ const myPeer = new Peer(undefined, {
 const myVideo = document.createElement("video");
 myVideo.muted = true;
 const peers = {};
+console.log(peers);
 navigator.mediaDevices
   .getUserMedia({
     video: true,
