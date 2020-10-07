274c13f9af5b89687021076878b6bbbac468b3d1
mattrbanks <banks.matt81@gmail.com>
2020-10-07 08:49:14 -0400

    Changed script.js to connect to self hosted peerjs server on heroku.

--- public/script.js
+++ public/script.js
@@ -1,10 +1,10 @@
 const socket = io("/");
 const videoGrid = document.getElementById("video-grid");
 const myPeer = new Peer(undefined, {
+  key: "peerjs",
   secure: true,
-  host: "https://face-conference.herokuapp.com/",
+  host: "https://face-conference-peerjs-server.herokuapp.com/",
   port: 443,
-  // port: 9000,
   // host: "/",
   // port: "3002",
 });
